ece2f53ae6611bb64d526e85678563492905f544
yosshiy02 <yosshiy02@gmail.com>
2026-05-20 11:28:36 +0900

    ff

---
+++ backend/shop_back/menu_back/HELMETTY_MENU.js
@@ -0,0 +1,25 @@
+export function getHelmettyMenuPayload() {
+    return {
+            brand: "HELMETTY",
+            brandPrefix: "he",
+            brandLogo: "https://static.wixstatic.com/media/414ae9_65ab64b531c549699eb7420ea84e0c95~mv2.jpg",
+            colorBoxBg: "#fff1f0",
+            colorBoxText: "#ff8acb",
+            colorBackBg: "#fffbd1",
+            colorBackBorder: "#ffd6d6",
+            colorBackText: "#e89aaa",
+            colorButtonBg: "#fff6f6",
+            colorButtonText: "#e68ab8",
+            colorButtonBorder: "#ffc7ee",
+            catchEyebrow: "HELMETTY ONLINE STORE",
+            catchMain: "今日の足もとに、ちょっとした遊び心を。",
+            catchSub: "かわいくて、気分が上がるシューズを集めました。",
+            linkTop: "/helmetty-top",
+            linkPolicy: "/policy",
+            linkForm: "/contact",
+            linkNotice: "/notice",
+            linkLink: "/onlinestore-top",
+            linkInsta: "https://www.instagram.com/helmetty_shop",
+            instaLink: "INSTAGRAM"
+        };
+}
